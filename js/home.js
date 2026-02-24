c72e5fecd64f08d3d020d11184cf4bbe04734ed5
lubna331 <lubna1077@gmail.com>
2026-02-24 06:25:58 +0600

    header boxes count done

--- js/home.js
+++ js/home.js
@@ -1,11 +1,26 @@
-console.log("hello there")
-// 1st >> get how many cards are there
-let cardsCount = document.getElementById("cards");
-// console.log(cardsCount.childElementCount);
-let totalCards = cardsCount.childElementCount;
-// then get the total element
-let total = document.getElementById("totalJob");
-total.innerText = totalCards;
-console.log(total.innerText);
+// console.log("hello there")
 
-// set cards number into total
+// get all the header boxes
+
+const totalCount = document.getElementById("totalCount")
+const interviewCount = document.getElementById("interviewCount")
+const rejectedCount = document.getElementById("rejectedCount")
+// console.log(totalCount);
+
+// get the card and children to count
+
+const getCards = document.getElementById("cards");
+
+// create function to count the card children
+
+function countChildren(){
+    //set total count's inner text
+    totalCount.innerText = getCards.children.length;
+}
+countChildren()
+
+
+// toggle buttons
+// const toggleAll = document.getElementById("toggle-all")
+// const toggleInterview = document.getElementById("toggle-interview")
+// const toggleRejected = document.getElementById("toggle-rejected");
